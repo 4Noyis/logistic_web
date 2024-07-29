a735f30ffd88e2b80a94fb5ca4d9a17823761df1
F.Ege Eryigit <f.ege.eryigit@gmail.com>
2024-07-29 09:00:40 +0300

    update settings.py for Media root, update medai urls in index.html, index.html works fine  - django

--- LOGISTIC_WEB/LOGISTIC_WEB/settings.py
+++ LOGISTIC_WEB/LOGISTIC_WEB/settings.py
@@ -126,6 +126,9 @@ STATICFILES_DIRS = [
     STATIC_DIR,
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = MEDIA_DIR
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
