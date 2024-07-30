374669e60619629eba08e9b44234a9fadb0016f1
F.Ege Eryigit <f.ege.eryigit@gmail.com>
2024-07-30 13:16:57 +0300

    add models to admin panel for check tables connection and records --> table relationships succes, admin panel success

--- LOGISTIC_WEB/order/admin.py
+++ LOGISTIC_WEB/order/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from order.models import Order,Receiving_location,Destination_location,Vehicle,Cargo,Bill
 
-# Register your models here.
+admin.site.register(Order)
+admin.site.register(Receiving_location)
+admin.site.register(Destination_location)
+admin.site.register(Vehicle)
+admin.site.register(Cargo)
+admin.site.register(Bill)
