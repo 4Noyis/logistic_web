8c88e3f17fbef1ea22fc2f36b272555b49ef0185
F.Ege Eryigit <f.ege.eryigit@gmail.com>
2024-08-11 14:35:43 +0300

    Map Update - Add Search bar

--- LOGISTIC_WEB/static/js/google_maps.js
+++ LOGISTIC_WEB/static/js/google_maps.js
@@ -5,27 +5,94 @@ const location_lng = location_data[0]['lng'];
 
 // Initialize and add the map
 let map;
+let marker;
+let infoWindow;
 
 async function initMap() {
+  // Request needed libraries.
+  //@ts-ignore
+  
+  const [{ Map }, { AdvancedMarkerElement }] = await Promise.all([
+    google.maps.importLibrary("marker"),
+    google.maps.importLibrary("places"),
+  ]);
+  const directionsService = new google.maps.DirectionsService(); //
+  const directionsRenderer = new google.maps.DirectionsRenderer(); //
 
-  const directionsService = new google.maps.DirectionsService();
-  const directionsRenderer = new google.maps.DirectionsRenderer();
+  // Initialize the map.
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 7,
-    center: { lat: 41.85, lng: -87.65 },
+    center: { lat: 40.749933, lng: -73.98633 },
+    zoom: 13,
+    mapId: "4504f8b37365c3d0",
+    mapTypeControl: false,
   });
 
-  directionsRenderer.setMap(map);
+  directionsRenderer.setMap(map); //
 
-  const onChangeHandler = function () {
-    calculateAndDisplayRoute(directionsService, directionsRenderer);
-  };
+  const onChangeHandler = function () {  //
+    calculateAndDisplayRoute(directionsService, directionsRenderer);//
+  };//
 
-  document.getElementById("start").addEventListener("change", onChangeHandler);
-  document.getElementById("end").addEventListener("change", onChangeHandler);
+  document.getElementById("start").addEventListener("change", onChangeHandler);//
+  document.getElementById("end").addEventListener("change", onChangeHandler);//
+
+  //@ts-ignore
+  const placeAutocomplete = new google.maps.places.PlaceAutocompleteElement();
+
+  //@ts-ignore
+  placeAutocomplete.id = "place-autocomplete-input";
+
+  const card = document.getElementById("place-autocomplete-card");
+
+  //@ts-ignore
+  card.appendChild(placeAutocomplete);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(card);
+  // Create the marker and infowindow
+  marker = new google.maps.marker.AdvancedMarkerElement({
+    map,
+  });
+  infoWindow = new google.maps.InfoWindow({});
+  // Add the gmp-placeselect listener, and display the results on the map.
+  //@ts-ignore
+  placeAutocomplete.addEventListener("gmp-placeselect", async ({ place }) => {
+    await place.fetchFields({
+      fields: ["displayName", "formattedAddress", "location"],
+    });
+    // If the place has a geometry, then present it on a map.
+    if (place.viewport) {
+      map.fitBounds(place.viewport);
+    } else {
+      map.setCenter(place.location);
+      map.setZoom(17);
+    }
+
+    let content =
+      '<div id="infowindow-content">' +
+      '<span id="place-displayname" class="title">' +
+      place.displayName +
+      "</span><br />" +
+      '<span id="place-address">' +
+      place.formattedAddress +
+      "</span>" +
+      "</div>";
+
+    updateInfoWindow(content, place.location);
+    marker.position = place.location;
+  });
 }
 
-function calculateAndDisplayRoute(directionsService, directionsRenderer) {
+// Helper function to create an info window.
+function updateInfoWindow(content, center) {
+  infoWindow.setContent(content);
+  infoWindow.setPosition(center);
+  infoWindow.open({
+    map,
+    anchor: marker,
+    shouldFocus: false,
+  });
+}
+
+function calculateAndDisplayRoute(directionsService, directionsRenderer) {//
   directionsService
     .route({
       origin: {
@@ -41,9 +108,9 @@ function calculateAndDisplayRoute(directionsService, directionsRenderer) {
       directionsRenderer.setDirections(response);
     })
     .catch((e) => window.alert("Directions request failed due to " + status));
-}
+}//
 
-window.initMap = initMap;
+initMap();
 
 /* ********* Simple Render for Selected Location *********
   // The location of Selected on map.py
